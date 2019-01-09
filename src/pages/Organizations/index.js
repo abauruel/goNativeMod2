0a6a131e6997aaeeb057f185ed042f6f659a8437
Alex Claude <abauruel@gmail.com>
2019-01-09 00:05:06 -0200

    adicionado consulta api github

--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
 import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import PropTypes from 'prop-types';
+import Header from '~/components/Header';
 
-// import styles from './styles';
+const Organizations = () => (
+  <View>
+    <Header title="Organizações" />
+  </View>
+);
+const TabIcon = ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />;
 
-const Organizations = () => <View />;
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Organizations.navigationOptions = {
+  tabBarIcon: TabIcon,
+};
 
 export default Organizations;
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { View } from 'react-native';
+import { View, Text, AsyncStorage, ActivityIndicator } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Header from '~/components/Header';
 
-const Repositories = () => (
-  <View>
-    <Header title="Repositories" />
-  </View>
-);
+import api from '~/services/api';
 
-export default Repositories;
+import styles from './styles'
+export default class Repositories extends Component {
+  static navigationOptions = {
+    tabBarIcon: ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />,
+  };
+
+  state = {
+    data: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const { data } = await api.get(`/users/${username}/repos`);
+
+    this.setState({
+      data,
+      loading: false,
+    });
+  }
+
+  renderList=()=>(
+    <Text>Lista 123</Text>
+  )
+
+  render() {
+    const { loading } = this.state;
+     return (
+      <View>
+        <Header title="Repositories" />
+        { loading ? <ActivityIndicator style={styles.loading}/> : this.renderList() }
+    </View>
+     )
+  }
+}
