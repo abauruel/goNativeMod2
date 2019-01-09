bf2a35a588a9254d5606019926988dcb407d4b41
Alex Claude <abauruel@gmail.com>
2019-01-08 22:47:28 -0200

    adicionado logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,39 @@
-import React from 'react';
-
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import { withNavigation } from 'react-navigation';
+import {
+  View, Text, TouchableOpacity, AsyncStorage,
+} from 'react-native';
 import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './styles';
 
-const Header = ({ title }) => (
-  <View style={styles.container}>
-    <View style={styles.title} />
-    <Text style={styles.left}>{title}</Text>
-    <TouchableOpacity onPress={() => {}} />
-  </View>
-);
+class Header extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
 
-Header.prototype = {
-  title: PropTypes.string.isRequired,
-};
-export default Header;
+  signOut = async () => {
+    const { navigation } = this.props;
+    await AsyncStorage.clear();
+
+    navigation.navigate('Welcome');
+  };
+
+  render() {
+    const { title } = this.props;
+    return (
+      <View style={styles.container}>
+        <View style={styles.title} />
+        <Text style={styles.left}>{title}</Text>
+        <TouchableOpacity onPress={this.signOut}>
+          <Icon name="exchange" size={16} style={styles.icon} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+export default withNavigation(Header);
